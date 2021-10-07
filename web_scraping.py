dfa5cadd0b9d85a3f61608f294189c711afe9a64
Wilson Lee <91751063+W1ls0nL44@users.noreply.github.com>
2021-10-07 01:01:25 -0400

    Update web_scraping.py

--- web_scraping.py
+++ web_scraping.py
@@ -6,10 +6,17 @@ def scrape(url):
     response = requests.get(source)
     soup = BeautifulSoup(response.text, "lxml")
 
-
-
+    #append attributes with same class tag and element tag into a list
+    #url = []
+    #for class_tag in soup.find all("a")  # a could be any class tag
+        e_tag = h2_tag.find('b') # b could be any element tag that inside the class tag
+        url.append(e_tag.attrs['abcd']  #append the the link of the element into url, abcd is the link attribute
+    #print(url)
+        
 def main():
     scrape()
 
 if __name__ == '__main__':
     main()
+
+# use .prettify() to print out nice formatted HTML
