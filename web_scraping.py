95faa988df77026f81fc4d8806c1857d0f844027
patelaks <patelaks119@gmail.com>
2021-10-22 11:54:44 -0400

    test commit

--- web_scraping.py
+++ web_scraping.py
@@ -3,34 +3,39 @@ from helium import *
 import time
 
 def scrape():
+
     # Set URL to scrape from
     url = 'https://sam.gov/content/home'
-    driver = start_chrome(url, headless = True) # web driver initialize
+
+    # Initialize Web Driver
+    driver = start_chrome(url, headless = True)
     driver.get(url)
 
+    # Timer for code to run periodically and pull new announcements
     time.sleep(5)
-
-    html=driver.page_source
-
-    # Parse and Save HTML to soup object
+@@ -19,18 +16,15 @@ def scrape():
     soup = BeautifulSoup(html, "html.parser")
 
-    announcments = soup.find("div", {"class" : "sds-feed"})
+    announcements = soup.find("div", {"class" : "sds-feed"})
+
+    # Filtering section
     article_links = []
-    for a in announcments.find_all('a', href=True):
+
+    # Check article for keyword, if present in text then append
+    for a in announcements.find_all('a', href=True):
         article_links.append(a['href'])
-    
+
+    # If keyword is not present in link, remove link
     for i in article_links:
         if (i.find('article') == -1):
             article_links.remove(i)
-    
+
+    # Prints article text
     for link in article_links:
-        print(link)  
-    
+        print(link)
 
 def main():
     scrape()
 
 if __name__ == '__main__':
     main()
-
