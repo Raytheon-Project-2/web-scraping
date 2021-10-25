240cc83880bdc7f70271bc582a89f80c63742340
Mattyc00 <mchoi1218@gmail.com>
2021-10-25 11:31:56 -0400

    added function to scrape article content

--- web_scraping.py
+++ web_scraping.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 from helium import *
 import time
 
-def scrape():
+def link_scrape():
     # Set URL to scrape from
     url = 'https://sam.gov/content/home'
     driver = start_chrome(url, headless = True) # web driver initialize
@@ -24,12 +24,33 @@ def scrape():
         if (i.find('article') == -1):
             article_links.remove(i)
     
-    for link in article_links:
-        print(link)  
-    
+    # for link in article_links:
+    #     print(link) 
+     
+    return article_links
+
+def article_content_scrape(links):   
+    for link in links:
+        print(link)
+        sub_driver = start_chrome(link, headless=True)
+        sub_driver.get(link)
+
+        time.sleep(5)
+
+        html = sub_driver.page_source
+        soup = BeautifulSoup(html, "html.parser")
+        # Government does not want me scraping their website boo :(
+        content = soup.body.text
+        #content = soup.find("article").find('section', {'class':'ng-binding ng-scope'}).find_all('p')
+        print(content)
+
+
+
+
 
 def main():
-    scrape()
+    article_links = link_scrape()
+    article_content_scrape(article_links)
 
 if __name__ == '__main__':
     main()
