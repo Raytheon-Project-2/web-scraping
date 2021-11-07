95963b3417bb74a9c1607c307345fd3f79a8f8d8
Mattyc00 <mchoi1218@gmail.com>
2021-11-07 14:03:10 -0500

    parse_doc function

--- browser_automation.py
+++ browser_automation.py
@@ -5,23 +5,24 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ChromeOptions
+import os
 
 def download():
     url = 'https://sam.gov/data-services/Contract%20Opportunities/Archived%20Data?privacy=Public'
 
     # Changes download directory
     c_options = ChromeOptions()
-    prefs = {"download.default_directory" : "C:\\Users\Matthew Choi\OneDrive\Purdue University\Fall 2021\DMCP Raytheon BI\BI_project_repo\web scraping\web-scraping"}
+    prefs = {"download.default_directory" : "C:\\Users\Matthew Choi\OneDrive\Purdue University\Fall 2021\DMCP Raytheon BI\BI_project_repo\web scraping\web-scraping\csvFiles"}
     c_options.add_experimental_option("prefs", prefs)
 
-    # Start an instance of chrome
-    driver = start_chrome(url, options=c_options) # add headless=True
-    driver.get(url)
+    for i in range(1,4): # change 4 to 29, probably move loop to line 16
+        # Start an instance of chrome
+        driver = start_chrome(url, options=c_options) # add headless=True
+        driver.get(url)
 
-    click(S('.usa-button'))
+        click(S('.usa-button'))
 
     # Downloads all of the files present
-    for i in range(1,4): # change 4 to 29, probably move loop to line 16
         WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "(//a[@class='data-service-file-link ng-star-inserted'])"+f'{[i]}'))).click()
 
         # Check and submit terms of service check boxes
@@ -30,11 +31,17 @@ def download():
         driver.find_element_by_xpath("//input[@id='term21']").click()
         click(S('.usa-modal-content-submit-btn'))
 
-    time.sleep(5)
-    kill_browser()
+        time.sleep(5)
+        kill_browser()
 
 def parse_docs():
-    with open("") as file:
+    dir_name = "csvFiles"
+    # establish database connection
+
+    for file in os.listdir(dir_name):
+        # read contents
+        # export as json
+        # dump into database
         NULL
 
 def main():
